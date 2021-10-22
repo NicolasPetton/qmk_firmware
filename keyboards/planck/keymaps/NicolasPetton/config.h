90bf5bd21dceecb0b52b75d29ae854392f4309dc
Nicolas Petton <nicolas@petton.fr>
2021-10-22 18:41:33 +0200

    Minor tweaks
    - Add auto-shift
    - merge FN and NUM layers together

--- keyboards/planck/keymaps/NicolasPetton/config.h
+++ keyboards/planck/keymaps/NicolasPetton/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2015-2021 Jack Humbert
+/* Copyright 2021 Nicolas Petton
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,35 +15,7 @@
  */
 
 #pragma once
-
-#ifdef AUDIO_ENABLE
-#    define STARTUP_SONG SONG(PLANCK_SOUND)
-// #define STARTUP_SONG SONG(NO_SOUND)
-
-#    define DEFAULT_LAYER_SONGS \
-        { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
-#endif
-
-/*
- * MIDI options
- */
-
-/* enable basic MIDI features:
-   - MIDI notes can be sent when in Music mode is on
-*/
-
-#define MIDI_BASIC
-
-/* enable advanced MIDI features:
-   - MIDI notes can be added to the keymap
-   - Octave shift and transpose
-   - Virtual sustain, portamento, and modulation wheel
-   - etc.
-*/
-//#define MIDI_ADVANCED
-
-/* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
-//#define MIDI_TONE_KEYCODE_OCTAVES 2
-
-// Most tactile encoders have detents every 4 stages
-#define ENCODER_RESOLUTION 4
+#define TAPPING_TERM 175
+#define AUTO_SHIFT_TIMEOUT 150
+/* #define AUTO_SHIFT_MODIFIERS */
+/* #define NO_AUTO_SHIFT_SPECIAL */
